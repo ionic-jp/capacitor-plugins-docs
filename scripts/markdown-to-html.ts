3dca830e37b4ef14a49d8d1b720e409683b3b54b
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-12-26 13:33:38 +0900

    feat(): enable to import plugin interfaces

--- scripts/markdown-to-html.ts
+++ scripts/markdown-to-html.ts
@@ -4,7 +4,8 @@ import { glob } from 'glob';
 import path from 'path';
 import { readFile, writeFile, mkdirp, remove } from 'fs-extra';
 import frontMatter from 'front-matter';
-import { MarkdownContent } from '../src/global/definitions';
+import {MarkdownContent, TypeObject} from '../src/global/definitions';
+import { getTypeToObjects } from './type-to-object';
 
 const globAsync = promisify(glob);
 
@@ -12,6 +13,8 @@ const DESTINATION_DIR = './src/assets/docs';
 const SOURCE_DIR = './src/docs';
 
 (async function () {
+  const docs: Record<'methods' | 'interfaces' | 'typeAliases' | 'enums', TypeObject[]> = getTypeToObjects();
+  const docsArr: TypeObject[] = Object.values(docs).flat(1);
   const files = await globAsync(`${SOURCE_DIR}/**/*.md`, {});
 
   await remove(DESTINATION_DIR);
@@ -27,8 +30,22 @@ const SOURCE_DIR = './src/docs';
 
     const markdownContents = await readFile(filePath, { encoding: 'utf8' });
     try {
-      let parsedMarkdown = frontMatter<any>(markdownContents);
-      const hypertext = markdownToHtml(parsedMarkdown.body.replace(/(<\/?docgen-index>|<\/?docgen-api>)/g, ''));
+      const parsedMarkdown = frontMatter<any>(markdownContents);
+      const splitBodyByLine = parsedMarkdown.body.split(/\r\n|\n/);
+
+      const parsedBody = splitBodyByLine.map(line => {
+        const result = line.match(/^!::([a-zA-Z0-9]+)::$/);
+        if (!result) {
+          return line;
+        }
+        const markdown = docsArr.find(doc => doc?.id === result[1])?.markdown;
+        if (markdown) {
+          return markdown;
+        }
+        throw result[1] + 'is not fount';
+      }).join('\n')
+
+      const hypertext = markdownToHtml(parsedBody);
 
       await mkdirp(path.join(DESTINATION_DIR, path.dirname(jsonFileName)));
       await writeFile(destinationFileName, JSON.stringify({
---
+++ scripts/type-to-object.ts
@@ -0,0 +1,149 @@
+import {DocsData, DocsInterfaceMethod, DocsMethodParam, TypeObject} from '../src/global/definitions';
+import * as dataSource from '@capacitor-community/stripe/dist/docs.json';
+import {formatDescription, formatType} from '@capacitor/docgen/dist/formatting';
+import {MarkdownTable} from '@capacitor/docgen/dist/markdown';
+import {DocsTagInfo} from '@capacitor/docgen';
+
+const formatMethodSignature = (m: DocsInterfaceMethod): string =>  {
+  if (m.name === 'addListener' && m.parameters.length > 0) {
+    return `addListener(${m.parameters[0].type.replace(/\"/g, `'`)}, ...)`;
+  }
+  return `${m.name}(${m.parameters.length > 0 ? '...' : ''})`;
+}
+
+const createMethodParamTable = (data: DocsData, parameters: DocsMethodParam[]): string => {
+  const t = new MarkdownTable();
+
+  t.addHeader([`Param`, `Type`, `Description`]);
+
+  parameters.forEach((p) => {
+    const nm = `**\`${p.name}\`**`;
+    const ty = formatType(data, p.type);
+    const docs = formatDescription(data, p.docs);
+    t.addRow([nm, ty.formatted, docs]);
+  });
+
+  t.removeEmptyColumns();
+  return codeToMarkdown(t.toMarkdown().join('\n'));
+}
+
+const getTagText = (tags: DocsTagInfo[], tagName: string): string =>  {
+  if (tags) {
+    const tag = tags.find((t) => t.name === tagName && typeof t.text === 'string');
+    if (tag) {
+      return tag.text!;
+    }
+  }
+  return '';
+}
+
+const codeToMarkdown = (text: string) => {
+  return text
+    .replace(/\<\/?code\>/g, `\``)
+    .replace(/\<("[^"]*"|'[^']*'|[^'">])*>/g, '');
+}
+
+const createMarkdown = (sources: TypeObject[], type: string) => {
+  return sources.map(data => {
+    return Object.assign(data, {
+      markdown: `#### \`${type}\` ${data.param}\n${
+        data.description ? data.description + '\n': ''
+      }${
+        data.type ? data.type  + '\n' : ''
+      }${
+        data.usage ? '##### Usage:\n```ts\n' + data.usage + '\n```'  + '\n' : ''
+      }`
+    });
+  })
+}
+
+export const getTypeToObjects = (): Record<'methods' | 'interfaces' | 'typeAliases' | 'enums', TypeObject[]> => {
+  const source = dataSource as DocsData;
+
+  const methods: TypeObject[] = source!.api!.methods.map((m: DocsInterfaceMethod): TypeObject => {
+    return {
+      id: m.name,
+      param: formatMethodSignature(m),
+      type: createMethodParamTable(source, m.parameters),
+      description: formatDescription(source, m.docs),
+      usage: `${m.name}${m.signature}`,
+    };
+  });
+
+  const interfaces: TypeObject[] = source.interfaces.map((i): TypeObject  => {
+    return {
+      id: i.name,
+      param: i.name,
+      type: (() => {
+        if (i.properties.length === 0) {
+          return '';
+        }
+        const t = new MarkdownTable();
+
+        t.addHeader([`Prop`, `Type`, `Description`, `Default`, `Since`]);
+
+        i.properties.forEach((m) => {
+          const defaultValue = getTagText(m.tags, 'default');
+
+          t.addRow([
+            `**\`${m.name}\`**`,
+            formatType(source, m.type).formatted,
+            formatDescription(source, m.docs),
+            defaultValue ? `<code>${defaultValue}</code>` : '',
+            getTagText(m.tags, 'since'),
+          ]);
+        });
+
+        t.removeEmptyColumns();
+        return codeToMarkdown(t.toMarkdown().join('\n'));
+      })(),
+      description: formatDescription(source, i.docs),
+    };
+  });
+
+  const typeAliases: TypeObject[] = source.typeAliases.map((t): TypeObject => {
+    return {
+      id: t.name,
+      param: t.name,
+      type: codeToMarkdown(t.types
+        .map((ty) => formatType(source, ty.text).formatted)
+        .join(' | ')
+        .replace(/\<\/code\> \| \<code\>/g, ` | `)),
+      description: formatDescription(source, t.docs),
+    };
+  });
+
+  const enums: TypeObject[] = source.enums.map((i): TypeObject => {
+    return {
+      id: i.name,
+      param: i.name,
+      type: (() => {
+        if (i.members.length === 0) {
+          return '';
+        }
+        const t = new MarkdownTable();
+
+        t.addHeader([`Members`, `Value`, `Description`, `Since`]);
+
+        i.members.forEach((m) => {
+          t.addRow([
+            `**\`${m.name}\`**`,
+            formatType(source, m.value).formatted.replace(/(\<\/code\> \| \<code\>)/g, `\``),
+            formatDescription(source, m.docs),
+            getTagText(m.tags, 'since'),
+          ]);
+        });
+
+        t.removeEmptyColumns();
+        return codeToMarkdown(t.toMarkdown().join('\n'));
+      })(),
+    };
+  });
+
+  return {
+    methods: createMarkdown(methods, 'method'),
+    interfaces: createMarkdown(interfaces, 'interface'),
+    typeAliases: createMarkdown(typeAliases, 'type alias'),
+    enums: createMarkdown(enums, 'enum'),
+  }
+}
