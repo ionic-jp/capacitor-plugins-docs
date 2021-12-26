08a8cc98e6ba784600bd211ac1aab8c2da802548
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2021-12-26 21:28:20 +0900

    fix(): observerの管理を修正

--- src/components/app-parser/app-parser.tsx
+++ src/components/app-parser/app-parser.tsx
@@ -1,4 +1,4 @@
-import {Component, Host, h, Prop, Element, Event, EventEmitter} from '@stencil/core';
+import {Component, Host, h, Prop, Element, Event, EventEmitter, State, Watch} from '@stencil/core';
 import {MarkdownContent} from '../../global/definitions';
 import { Build } from '@stencil/core';
 
@@ -11,11 +11,28 @@ export class AppParser {
   @Element() el: HTMLElement;
   @Prop() markdownContent: MarkdownContent;
   @Prop() hideCodeBlock: boolean;
+  @State() _markdownContent: MarkdownContent;
   @Event({
     eventName: 'changedActiveLine',
   }) changedActiveLine: EventEmitter<Record<string, number[]>>;
+  private observers: IntersectionObserver[] = [];
+
+  @Watch('markdownContent')
+  async watchLabelHandler() {
+    this._markdownContent = this.markdownContent;
+  }
 
   componentDidLoad() {
+    this.prepareIntersectionObserver();
+  }
+
+  componentDidUpdate() {
+    this.observers.forEach(observer => observer.disconnect());
+    this.observers = [];
+    this.prepareIntersectionObserver();
+  }
+
+  prepareIntersectionObserver() {
     if (this.markdownContent.scrollActiveLine?.length > 0) {
       const options = {
         // root: document.querySelector('.root'),
@@ -40,20 +57,19 @@ export class AppParser {
         });
       };
 
-      
-      this.markdownContent.scrollActiveLine.map(item => {
+      const targets: HTMLElement[] = this.markdownContent.scrollActiveLine.map(item => {
         if (Build.isBrowser) {
-          const target = !item.id ?
+          return !item.id ?
             this.el.shadowRoot.querySelector('h1') :
             this.el.shadowRoot.getElementById(item.id.toLowerCase().replace(' ', '-'));
-          console.log(target);
-          if (target) {
-            const observer = new IntersectionObserver(callback, options);
-            observer.observe(target);
-          }
         }
       });
 
+      this.observers = targets.map(target => {
+        const observer = new IntersectionObserver(callback, options)
+        observer.observe(target);
+        return observer;
+      });
     }
   }
 
