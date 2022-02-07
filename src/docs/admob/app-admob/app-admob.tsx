c67758326bca456305b36a91da8b10755b84e35c
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2022-02-07 11:53:59 +0900

    feat(): first step of adding new plugin

---
+++ src/docs/admob/app-admob/app-admob.tsx
@@ -0,0 +1,16 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-admob',
+  styleUrl: 'app-admob.scss',
+  shadow: true,
+})
+export class AppAdmob {
+  render() {
+    return (
+      <div>
+        Hello World!!
+      </div>
+    );
+  }
+}
